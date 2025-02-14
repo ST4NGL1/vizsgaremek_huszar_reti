ff333aef7f5ed7c8991f97f183ba0ca62a1ef25b
Martin <huszmartin2005@gmail.com>
2025-02-14 19:05:15 +0100

    MACBOOK proba

--- Assets/php/db_connect.php
+++ Assets/php/db_connect.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="restaurant_project";
+$db="restaurant";
 $conn=new mysqli($host,$user,$pass,$db);
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
